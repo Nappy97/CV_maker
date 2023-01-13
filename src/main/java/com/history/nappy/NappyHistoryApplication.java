3c245bd3de70d3827aafb206b9cabd9cc5684058
Nappy97 <wjddmlgud829@gmail.com>
2023-01-13 11:28:38 +0900

    Initial commit

---
+++ src/main/java/com/history/nappy/NappyHistoryApplication.java
@@ -0,0 +1,13 @@
+package com.history.nappy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NappyHistoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NappyHistoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/history/nappy/NappyHistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.history.nappy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NappyHistoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
