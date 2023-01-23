2ff47bd7d72e9c9dcb3308e95fb49c95c99934df
Nappy97 <wjddmlgud829@gmail.com>
2023-01-23 18:25:47 +0900

    0123

--- src/main/java/com/history/nappy/service/cv/CVService.java
+++ src/main/java/com/history/nappy/service/cv/CVService.java
@@ -1,5 +1,8 @@
 package com.history.nappy.service.cv;
 
+import com.history.nappy.domain.cv.CV;
+import com.history.nappy.domain.member.Member;
+import com.history.nappy.dto.cv.CVDetailsDto;
 import com.history.nappy.repository.cv.CVDetailsRepository;
 import com.history.nappy.repository.cv.CVRepository;
 import com.history.nappy.repository.member.MemberRepository;
@@ -16,5 +19,15 @@ public class CVService {
     private final CVDetailsRepository cvDetailsRepository;
     private final MemberRepository memberRepository;
 
+    // CV detail 추가
+    public Long addDetail(CVDetailsDto cvDetailsDto, String username){
 
+        Member member = memberRepository.findByUsername(username);
+        CV cv = cvRepository.findByMemberId(member.getId());
+
+        // 비어있나요?
+        if (cv == null){
+
+        }
+    }
 }
